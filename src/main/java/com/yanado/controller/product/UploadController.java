7b82517a8edb76c2700688b677578ba4f67a6189
HYUNJEONGMUN <s205044s@naver.com>
2021-10-05 03:15:48 +0900

    경로 수정

--- src/main/java/com/yanado/controller/product/UploadController.java
+++ src/main/java/com/yanado/controller/product/UploadController.java
@@ -18,100 +18,96 @@ import com.yanado.dto.Photo;
 
 @Controller
 public class UploadController {
-	
+
 	@RequestMapping("/photoUpload")
-	public String photoUpload(HttpServletRequest request, Photo vo){
-	    String callback = vo.getCallback();
-	    String callback_func = vo.getCallback_func();
-	    String file_result = "";
-	    try {
-	        if(vo.getFiledata() != null && vo.getFiledata().getOriginalFilename() != null && !vo.getFiledata().getOriginalFilename().equals("")){
-	            //파일이 존재하면
-	            String original_name = vo.getFiledata().getOriginalFilename();
-	            String ext = original_name.substring(original_name.lastIndexOf(".")+1);
-	            //파일 기본경로
-	            String defaultPath = request.getSession().getServletContext().getRealPath("/");
-	            //파일 기본경로 _ 상세경로
-	            String path =  "/test/WEB-INF/classes/static/productImage" +  File.separator;         
-	            File file = new File(path);
-	            System.out.println("path:"+path);
-	            //디렉토리 존재하지 않을경우 디렉토리 생성
-	            if(!file.exists()) {
-	                file.mkdirs();
-	            }
-	            //서버에 업로드 할 파일명(한글문제로 인해 원본파일은 올리지 않는것이 좋음)
-	            String realname = UUID.randomUUID().toString() + "." + ext;
-	        ///////////////// 서버에 파일쓰기 /////////////////
-	            vo.getFiledata().transferTo(new File(path+realname));
-	            file_result += "&bNewLine=true&sFileName="+original_name+"&sFileURL=../../static/productImage/"+realname;
-	        } else {
-	            file_result += "&errstr=error";
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return "redirect:" + callback + "?callback_func="+callback_func+file_result;
+	public String photoUpload(HttpServletRequest request, Photo vo) {
+		String callback = vo.getCallback();
+		String callback_func = vo.getCallback_func();
+		String file_result = "";
+		try {
+			if (vo.getFiledata() != null && vo.getFiledata().getOriginalFilename() != null
+					&& !vo.getFiledata().getOriginalFilename().equals("")) {
+			
+				String original_name = vo.getFiledata().getOriginalFilename();
+				String ext = original_name.substring(original_name.lastIndexOf(".") + 1);
+			
+				String defaultPath = request.getSession().getServletContext().getRealPath("/");
+			
+				String path = "/static/productImage" + File.separator;
+				File file = new File(path);
+				System.out.println("path:" + path);
+				
+				if (!file.exists()) {
+					file.mkdirs();
+				}
+				String realname = UUID.randomUUID().toString() + "." + ext;
+				vo.getFiledata().transferTo(new File(path + realname));
+				file_result += "&bNewLine=true&sFileName=" + original_name + "&sFileURL=../../static/productImage/"
+						+ realname;
+			} else {
+				file_result += "&errstr=error";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:" + callback + "?callback_func=" + callback_func + file_result;
 	}
-	
-	//다중파일업로드
+
+	// 다중파일업로드
 	@RequestMapping("/multiplePhotoUpload")
-	public void multiplePhotoUpload(HttpServletRequest request, HttpServletResponse response){
-	    try {
-	    	
-	    	System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-	    	
-	         //파일정보
-	         String sFileInfo = "";
-	         //파일명을 받는다 - 일반 원본파일명
-	         String filename = request.getHeader("file-name");
-	         //파일 확장자
-	         String filename_ext = filename.substring(filename.lastIndexOf(".")+1);
-	         //확장자를소문자로 변경
-	         filename_ext = filename_ext.toLowerCase();
-	         //파일 기본경로
-	         String dftFilePath = request.getSession().getServletContext().getRealPath("/");
-	         //파일 기본경로 _ 상세경로
-	         String filePath  =  "/test/WEB-INF/classes/static/productImage" +  File.separator;
-	         
-	         System.out.println(filePath);
-	         
-	         File file = new File(filePath);
-	         if(!file.exists()) {
-	            file.mkdirs();
-	         }
-	         
-	         System.out.println();
-	         
-	         String realFileNm = "";
-	         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-	         String today= formatter.format(new java.util.Date());
-	         realFileNm = today+UUID.randomUUID().toString() + filename.substring(filename.lastIndexOf("."));
-	         String rlFileNm = filePath + realFileNm;
-	         ///////////////// 서버에 파일쓰기 /////////////////
-	         InputStream is = request.getInputStream();
-	         OutputStream os=new FileOutputStream(rlFileNm);
-	         int numRead;
-	         byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
-	         while((numRead = is.read(b,0,b.length)) != -1){
-	            os.write(b,0,numRead);
-	         }
-	         if(is != null) {
-	            is.close();
-	         }
-	         os.flush();
-	         os.close();
-	         ///////////////// 서버에 파일쓰기 /////////////////
-	         // 정보 출력
-	         sFileInfo += "&bNewLine=true";
-	         // img 태그의 title 속성을 원본파일명으로 적용시켜주기 위함
-	         sFileInfo += "&sFileName="+ filename;;
-	         sFileInfo += "&sFileURL="+"../../static/productImage/"+realFileNm;
-	         PrintWriter print = response.getWriter();
-	         print.print(sFileInfo);
-	         print.flush();
-	         print.close();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
+	public void multiplePhotoUpload(HttpServletRequest request, HttpServletResponse response) {
+		try {
+
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+			String sFileInfo = "";
+
+			String filename = request.getHeader("file-name");
+
+			String filename_ext = filename.substring(filename.lastIndexOf(".") + 1);
+
+			filename_ext = filename_ext.toLowerCase();
+
+			String dftFilePath = request.getSession().getServletContext().getRealPath("/");
+
+			String filePath = "/static/productImage" + File.separator;
+
+			System.out.println(filePath);
+
+			File file = new File(filePath);
+			if (!file.exists()) {
+				file.mkdirs();
+			}
+
+			String realFileNm = "";
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+			String today = formatter.format(new java.util.Date());
+			realFileNm = today + UUID.randomUUID().toString() + filename.substring(filename.lastIndexOf("."));
+			String rlFileNm = filePath + realFileNm;
+
+			InputStream is = request.getInputStream();
+			OutputStream os = new FileOutputStream(rlFileNm);
+			int numRead;
+			byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+			while ((numRead = is.read(b, 0, b.length)) != -1) {
+				os.write(b, 0, numRead);
+			}
+			if (is != null) {
+				is.close();
+			}
+			os.flush();
+			os.close();
+
+			sFileInfo += "&bNewLine=true";
+			sFileInfo += "&sFileName=" + filename;
+			
+			sFileInfo += "&sFileURL=" + "../../static/productImage/" + realFileNm;
+			PrintWriter print = response.getWriter();
+			print.print(sFileInfo);
+			print.flush();
+			print.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
