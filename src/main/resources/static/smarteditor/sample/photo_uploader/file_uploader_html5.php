d8e4c400a7d0337eabf3f330481ba5950d6c39f2
HYUNJEONGMUN <s205044s@naver.com>
2021-10-05 00:11:20 +0900

    image

--- src/main/resources/static/smarteditor/sample/photo_uploader/file_uploader_html5.php
+++ src/main/resources/static/smarteditor/sample/photo_uploader/file_uploader_html5.php
@@ -1,39 +1,50 @@
 <?php
- 	$sFileInfo = '';
-	$headers = array();
-	 
-	foreach($_SERVER as $k => $v) {
-		if(substr($k, 0, 9) == "HTTP_FILE") {
-			$k = substr(strtolower($k), 5);
-			$headers[$k] = $v;
-		} 
-	}
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Max-Age: 86400');
+header('Access-Control-Allow-Headers: x-requested-with');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
-	$filename = rawurldecode($headers['file_name']);
-	$filename_ext = strtolower(array_pop(explode('.',$filename)));
-	$allow_file = array("jpg", "png", "bmp", "gif"); 
+$sFileInfo = '';
+$headers = array();
 
-	if(!in_array($filename_ext, $allow_file)) {
-		echo "NOTALLOW_".$filename;
-	} else {
-		$file = new stdClass;
-		$file->name = date("YmdHis").mt_rand().".".$filename_ext;
-		$file->content = file_get_contents("php://input");
+foreach ($_SERVER as $k => $v) {
+    if (substr($k, 0, 9) == "HTTP_FILE") {
+        $k = substr(strtolower($k), 5);
+        $headers[$k] = $v;
+    }
+}
 
-		$uploadDir = '../../upload/';
-		if(!is_dir($uploadDir)){
-			mkdir($uploadDir, 0777);
-		}
-		
-		$newPath = $uploadDir.$file->name;
-		
-		if(file_put_contents($newPath, $file->content)) {
-			$sFileInfo .= "&bNewLine=true";
-			$sFileInfo .= "&sFileName=".$filename;
-			//$sFileInfo .= "&sFileURL=/smarteditor/upload/".$file->name;
-			$sFileInfo .= '&sFileURL=http://152.70.254.64:8080/static/smarteditor/upload'. urlencode(urlencode($name));
-			// $sFileInfo .= '&sFileURL=http://localhost:8080/static/smarteditor/upload'. urlencode(urlencode($name));
-		}
-		echo $sFileInfo;
-	}
+$filename = rawurldecode($headers['file_name']);
+$filename_ext = strtolower(array_pop(explode('.', $filename)));
+$allow_file = array(
+    "jpg",
+    "png",
+    "bmp",
+    "gif"
+);
+
+if (! in_array($filename_ext, $allow_file)) {
+    echo "NOTALLOW_" . $filename;
+} else {
+    $file = new stdClass();
+    $file->name = date("YmdHis") . mt_rand() . "." . $filename_ext;
+    $file->content = file_get_contents("php://input");
+
+    $uploadDir = '../../upload/';
+    if (! is_dir($uploadDir)) {
+        mkdir($uploadDir, 0777);
+    }
+
+    $newPath = $uploadDir . $file->name;
+
+    if (file_put_contents($newPath, $file->content)) {
+        $sFileInfo .= "&bNewLine=true";
+        $sFileInfo .= "&sFileName=" . $filename;
+        // $sFileInfo .= "&sFileURL=/smarteditor/upload/".$file->name;
+      //  $sFileInfo .= '&sFileURL=http://152.70.254.64:8080/static/smarteditor/upload' . urlencode(urlencode($name));
+        $sFileInfo .= '&sFileURL=http://localhost:8080/static/smarteditor/upload'. urlencode(urlencode($name));
+    }
+
+    echo $sFileInfo;
+}
 ?>
